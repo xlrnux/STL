f4cd40b99229eb76c63001dfc7b13179d0543ddc
HyundongLim <thunderfry89@gmail.com>
2019-05-04 01:50:53 +0900

    리사이징 버그 픽스 Set 함수 만들고 사용.

--- SimpleHashTable.h
+++ SimpleHashTable.h
@@ -29,144 +29,163 @@ static void print_Prime(){
 
 //static int Prime[] = {9,10007,20021,40063,80141,160309,320627,641261,1282529,2565061,5130143,10260301};
 // TableSize (겸 해싱에서 나머지 연산의 피연산자로 사용될 목록)
-static int Prime[] = {9,10007,20021,40063,80141,160309,320627,641261}; // 현실적인 사이즈
+static int Prime[] = {10007,20021,40063,80141,160309,320627,641261}; // 현실적인 사이즈
 class Map{
 public:
     typedef int KeyType; // Template 대신 아쉬운대로
     typedef int ValueType; // Template 대신 아쉬운대로
+
     typedef struct Node{ // 우리의 주인공님
-        KeyType Key;
-        ValueType Value;
-        Node* Next;
+        KeyType Key; // 진짜 키값
+        ValueType Value; // 값
+        Node* Next; // 체이닝을 위한 다음노드
+        Node(){ // 자동 초기화. 아마 C에서는 못쓸듯
+            this->Key = -1;
+        }
     }Node;
 private:
     int SizeIdx = 0; // TableSize = Prime[SizeIdx]
-    int TableSize = Prime[SizeIdx];
-    Node* Table = new Node[TableSize];
-    int UsedSize = 0;
-    int newSizeIdx = 1;
-    int newTableSize = Prime[newSizeIdx];
-    int oldTableSize = 0;
-    int ChainLimit = 5;
-    bool rehash = false;
+    int newSizeIdx = SizeIdx + 1; // 리사이징 대비
+
+    int TableSize = Prime[SizeIdx]; // 배열 사이즈 겸, 해싱용 피연산자
+    int oldTableSize = 0; // 리사이징 대비
+    int newTableSize = Prime[newSizeIdx]; // 리사이징 대비
+
+    Node* Table = new Node[TableSize]; // 해시 테이블 배열
+    int UsedSize = 0; // 사용된 공간 카운트
+
+    int ChainLimit = 4; // 클러스터 최대 허용값. 해당 값을 초과하면 리사이징
+    bool rehash = false; // 리사이징 필요여부 파라미터
 public:
     Map(){
-        for(int i = 0; i < TableSize; i++) {
-            Table[i] = (Node){-1,-1,NULL};
-        }
-    }
 
+    }
     ~Map(){
+        // 해시맵 동적할당 해제
         for(int i = 0; i < this->TableSize; i++){
-            if(Table[i].Key == -1) continue;
+            if(Table[i].Next == NULL) continue; // 사용되지 않은 원소. 반드시 continue
             Node* Current = Table[i].Next;
             KeyType Key = Current->Key;
-            while( Current->Next != NULL ) {
+            while( Current != NULL ) {
                 Node* next = Current->Next;
                 Current->Next = NULL;
                 delete Current;
                 Current = next;
             }
-            if(Current->Key == -1) delete Current;
         }
         delete[] Table;
+    }
+
+    void Stats(){
+        std::cout << "==== start print stats ! \n";
+        std::cout << "SizeIdx: " << SizeIdx;
+        std::cout << " TableSize: " << TableSize;
+        std::cout << " UsedSize: " << UsedSize;
+        std::cout << " ChainLimit: " << ChainLimit;
+
+        std::cout << "\n";
+
+        std::cout << "==== end print stats ! \n";
+    }
+    void FullPrint(){
+        Stats();
+        std::cout << "-----------start print! \n";
+        for(int i = 0; i < TableSize; i++){
+            Node* Current = &Table[i];
+            while(Current->Key != -1){
+                std::cout << "TableSize: " << TableSize << "\t";
+                std::cout << "Address: " << Hash(Current->Key);
+                std::cout << " Key: " << Current->Key;
+                std::cout << " Val: " << Current->Value << "\n";
+                Current = Current->Next;
+            }
+            if(Table[i].Key != -1) std::cout << "\n";
+        }
+        std::cout << "-----------end print! \n";
     }
 
     int GetTableSize(){
         return this->TableSize;
     }
+
     int Hash(KeyType Key){
         // Key 로부터 Address 를 계산
-        // Key 가 음수일 경우 재할당중이니 newTableSize 기준으로 나머지
-        // 키의 기본값이 -1인만큼 이것도 신경써야할듯
-        // 통상시에 키에 음수가 안들어 가도록 각별히 주의를 해야할듯
-        if(Key < 0) return (Key*-1) % newTableSize; // 재할당중
         return Key % TableSize;
     }
 
     ValueType& operator [](KeyType Key){
-        if( (this->UsedSize)*1.0 >= TableSize*0.75 // 총 용량의 75퍼센트 이상을 사용
+        if( (this->UsedSize)*1.0 >= (this->TableSize)*0.75 // 총 용량의 75퍼센트 이상을 사용
         || rehash){ // 체이닝이 너무 커짐
-            rehash = false;
-            ChainLimit *= 5;
-            if(DEBUG) std::cout << "ReSized!!!\n";
-            this->oldTableSize = this->TableSize;
-            this->SizeIdx = this->newSizeIdx;
-            this->TableSize = this->newTableSize;
-            this->newSizeIdx = this->SizeIdx+1;
-            this->newTableSize = Prime[newSizeIdx];
             ReSize();
         }
 
-
         int Address = Hash(Key);
-        if(Key < 0) Key *= -1;// Key가 음수라면 재할당중 호출
-        // 키의 기본값이 -1인만큼 이것도 신경써야할듯
-        // 통상시에 키에 음수가 안들어 가도록 각별히 주의를 해야할듯
+        Node* Current = &Table[Address];
 
+        int chain = 0;
+        while( Current->Next != NULL
+        && ++chain) {
+            if (Current->Key == Key) return Current->Value;
 
-        while(true){
-            Node* Current = &Table[Address];
-            int turn = 0;
-            while(++turn) {
-                if (Current->Key == Key) {
-                    return Current->Value;
-                } else if (Current->Next == NULL) {
-                    if(turn > 1) {
-                        // 체이닝이 아닌 최초 노드에 삽입 된 경우에는 UsedSize 를 더해준다                        //
-                        this->UsedSize++;
-                        if(turn > ChainLimit) {
-                            rehash = true;
-                        }
-                    }
-                    Current->Key = Key;
-                    Node* NewNode = new Node;
-                    NewNode->Key = -1;
-                    NewNode->Next = NULL;
-                    Current->Next = NewNode;
-                    if(DEBUG) std::cout << turn << "\n";
-                    return Current->Value;
-                }  else {
-                    Current = Current->Next;
-                }
-            }
+            Current = Current->Next;
         }
+
+        // 체이닝이 아닌 최초 노드에 삽입 된 경우에는 UsedSize 를 더해준다
+        if(chain == 0) this->UsedSize++;
+        if(chain >= ChainLimit) rehash = true;
+
+        Current->Key = Key;
+        Current->Next = new Node();
+        return Current->Value;
     }
 
 private:
-    int minus(int num){
-        return num*-1;
+    void Set(KeyType Key, ValueType Value){
+        // 재 해싱할 때 옮기는 용도로만 사용함
+        int chain = 0;
+        int Address = Hash(Key);
+        Node* Current = &Table[Address];
+        while(Current->Next != NULL && ++chain){
+            Current = Current->Next;
+        }
+        Current->Key = Key;
+        Current->Value = Value;
+        Current->Next = new Node();
+        if(chain == 0) this->UsedSize++;
+        if(chain >= this->ChainLimit) rehash = true;
     }
+
     void ReSize() {
+        rehash = false;
+        ChainLimit *= 5;
+        if(DEBUG) std::cout << "ReSized!!!\n";
+        this->SizeIdx = this->newSizeIdx;
+        this->newSizeIdx = this->SizeIdx+1;
+        this->oldTableSize = this->TableSize;
+        this->TableSize = this->newTableSize;
+        this->newTableSize = Prime[newSizeIdx];
+        this->UsedSize = 0;
 
         Node* oldTable = Table;
         Table = new Node[TableSize];
 
-        for(int i = 0; i < TableSize; i++) {
-            Table[i].Key = -1;
-            Table[i].Next = NULL;
-        }
-
-        int Address;
         for(int i = 0; i < this->oldTableSize; i++){
-            if((oldTable[i]).Key == -1) continue;
-            Address = Hash( minus(oldTable[i].Key) );
-            Table[Address].Value = (oldTable[i]).Value;
+            if( oldTable[i].Key == -1) continue;
 
-            Node* Current = (oldTable[i]).Next;
-            KeyType Key = Current->Key;
-            while( Current->Next != NULL ) {
-                Address = Hash( minus(Current->Key) );
-                Table[Address].Value = Current->Value;
+            Set(oldTable[i].Key, oldTable[i].Value);
+            Node* Current = oldTable[i].Next;
+            while( Current->Key != -1 ) {
+                Set(Current->Key, Current->Value);
 
                 Node* next = Current->Next;
                 Current->Next = NULL;
                 delete Current;
                 Current = next;
             }
-            if(Current->Key == -1) delete Current;
+            delete Current;
         }
-        delete[] (oldTable);
+
+        delete[] oldTable;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "SimpleHashTable.h"
-#include<cstring>
-
 
 int main(void) {
     Map map1;
+    map1.Stats();
+
 
     map1[418] = 32114;
     map1[9] = 514;
@@ -14,38 +14,30 @@ int main(void) {
     map1[28+map1.GetTableSize()*2] = 286;
     map1[28+map1.GetTableSize()*3] = 386;
     map1[28+map1.GetTableSize()*4] = 486;
-    printf("-----------------------\n");
-    printf("Key:%d\t\tValue:%d\n", 418 , map1[418] );
-    printf("Key:%d\t\tValue:%d\n", 9 , map1[9] );
-    printf("Key:%d\t\tValue:%d\n", 27 , map1[27] );
-    printf("Key:%d\tValue:%d\n", 28 , map1[28] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize() , map1[28+map1.GetTableSize()] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*2 , map1[28+map1.GetTableSize()*2] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*3 , map1[28+map1.GetTableSize()*3] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*4 , map1[28+map1.GetTableSize()*4] );
+    map1.FullPrint();
+
     map1[28] = 76;
     map1[28+map1.GetTableSize()] = 176;
     map1[28+map1.GetTableSize()*2] = 276;
     map1[28+map1.GetTableSize()*3] = 376;
     map1[28+map1.GetTableSize()*4] = 476;
-    printf("Key:%d\tValue:%d\n", 28 , map1[28] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize() , map1[28+map1.GetTableSize()] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*2 , map1[28+map1.GetTableSize()*2] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*3 , map1[28+map1.GetTableSize()*3] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*4 , map1[28+map1.GetTableSize()*4] );
-    printf("Key:%d\tValue:%d\n", 28 , map1[28] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize() , map1[28+map1.GetTableSize()] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*2 , map1[28+map1.GetTableSize()*2] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*3 , map1[28+map1.GetTableSize()*3] );
-    printf("Key:%d\tValue:%d\n", 28+map1.GetTableSize()*4 , map1[28+map1.GetTableSize()*4] );
-
-    //int Prime[1000000] = {0,};
+    map1.FullPrint();
 
     map1[282] = 76;
-    map1[282+map1.GetTableSize()] = 176;
-    map1[282+map1.GetTableSize()*2] = 276;
-    map1[282+map1.GetTableSize()*3] = 376;
-    map1[28+map1.GetTableSize()*4] = 476;
+    map1[282+map1.GetTableSize()*2] = 266;
+    map1[282+map1.GetTableSize()*3] = 366;
+    map1[282+map1.GetTableSize()] = 166;
+    map1[28+map1.GetTableSize()*4] = 466;
+    map1.FullPrint();
+
+    /*
+    for(int i = 0; i < map1.GetTableSize(); i++){
+        if(map1[i] == 0 ) continue;
+        printf("%d %d\n", i, map1[i]);
+    }
+*/
+
+    //map1.FullPrint();
 
     return 0;
 }
